bf93e5fee912ee8ecde89b76520f2ba66787e1dd
FantasyGmm <16450052+FantasyGmm@users.noreply.github.com>
2020-03-10 17:42:34 +0800

    添加项目文件。

---
+++ MainForm.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO;
+using ImageMagick;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+
+namespace PackImage
+{
+    public partial class MainForm : Form
+    {
+        private const byte PACK_SPACE = 0xF;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog
+            {
+                ShowNewFolderButton = false
+            };
+            fbd.ShowDialog();
+            textBox1.Text = fbd.SelectedPath;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != string.Empty)
+            {
+                MemoryStream ms = new MemoryStream();
+                foreach (var file in Directory.GetFiles(textBox1.Text))
+                {
+                    byte[] tmp = AuthGetFileData(file);
+                    foreach (var btmp in tmp)
+                    {
+                        ms.WriteByte(btmp);
+                    }
+                    ms.WriteByte(PACK_SPACE);
+                }
+                if (File.Exists("pack.dat"))
+                    File.Delete("pack.dat");
+                FileStream fs = new FileStream("pack.dat",FileMode.Create);
+                fs.Write(ms.GetBuffer(),0,ms.GetBuffer().Length);
+                fs.Dispose();
+                ms.Dispose();
+                Process.Start(Directory.GetCurrentDirectory());
+            }
+        }
+        protected byte[] AuthGetFileData(string fileUrl)
+        {
+            using (FileStream fs = new FileStream(fileUrl, FileMode.Open, FileAccess.Read))
+            {
+                byte[] buffur = new byte[fs.Length];
+                using (BinaryWriter bw = new BinaryWriter(fs))
+                {
+                    bw.Write(buffur);
+                    bw.Close();
+                }
+                return buffur;
+            }
+        }
+        public static byte[] GetSingleBitmap(string file)
+        {
+            Bitmap pimage = new Bitmap(file);
+            Bitmap source;
+
+            // If original bitmap is not already in 32 BPP, ARGB format, then convert
+            if (pimage.PixelFormat != PixelFormat.Format32bppArgb)
+            {
+                source = new Bitmap(pimage.Width, pimage.Height, PixelFormat.Format32bppArgb);
+                source.SetResolution(pimage.HorizontalResolution, pimage.VerticalResolution);
+                using (Graphics g = Graphics.FromImage(source))
+                {
+                    g.DrawImageUnscaled(pimage, 0, 0);
+                }
+            }
+            else
+            {
+                source = pimage;
+            }
+
+            // Lock source bitmap in memory
+            BitmapData sourceData = source.LockBits(new Rectangle(0, 0, source.Width, source.Height),
+                ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+            // Copy image data to binary array
+            int imageSize = sourceData.Stride * sourceData.Height;
+            byte[] sourceBuffer = new byte[imageSize];
+            Marshal.Copy(sourceData.Scan0, sourceBuffer, 0, imageSize);
+
+            // Unlock source bitmap
+            source.UnlockBits(sourceData);
+
+            // Create destination bitmap
+            System.Drawing.Bitmap destination =
+                new System.Drawing.Bitmap(source.Width, source.Height, PixelFormat.Format1bppIndexed);
+
+            // Lock destination bitmap in memory
+            BitmapData destinationData =
+                destination.LockBits(new Rectangle(0, 0, destination.Width, destination.Height),
+                    ImageLockMode.WriteOnly, PixelFormat.Format1bppIndexed);
+
+            // Create destination buffer
+            imageSize = destinationData.Stride * destinationData.Height;
+            byte[] destinationBuffer = new byte[imageSize];
+            int height = source.Height;
+            int width = source.Width;
+            int threshold = 500;
+
+            // Iterate lines
+            for (int y = 0; y < height; y++)
+            {
+                int sourceIndex = y * sourceData.Stride;
+                int destinationIndex = y * destinationData.Stride;
+                byte destinationValue = 0;
+                int pixelValue = 128;
+
+                // Iterate pixels
+                for (int x = 0; x < width; x++)
+                {
+                    // Compute pixel brightness (i.e. total of Red, Green, and Blue values)
+                    int pixelTotal = sourceBuffer[sourceIndex + 1] + sourceBuffer[sourceIndex + 2] +
+                                     sourceBuffer[sourceIndex + 3];
+                    if (pixelTotal > threshold)
+                    {
+                        destinationValue += (byte)pixelValue;
+                    }
+
+                    if (pixelValue == 1)
+                    {
+                        destinationBuffer[destinationIndex] = destinationValue;
+                        destinationIndex++;
+                        destinationValue = 0;
+                        pixelValue = 128;
+                    }
+                    else
+                    {
+                        pixelValue >>= 1;
+                    }
+
+                    sourceIndex += 4;
+                }
+
+                if (pixelValue != 128)
+                {
+                    destinationBuffer[destinationIndex] = destinationValue;
+                }
+            }
+
+            // Copy binary image data to destination bitmap
+            Marshal.Copy(destinationBuffer, 0, destinationData.Scan0, imageSize);
+
+            // Unlock destination bitmap
+            destination.UnlockBits(destinationData);
+
+            // Dispose of source if not originally supplied bitmap
+            if (source != pimage)
+            {
+                source.Dispose();
+            }
+
+            MemoryStream ms = new MemoryStream();
+            destination.Save(ms, ImageFormat.Bmp);
+            return ms.ToArray();
+        }
+    }
+}
